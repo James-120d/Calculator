6c67bf14a33e1af9ab2c0322c60e739d0141ca03
James McGonagle <jamesmcg08@hotmail.co.uk>
2025-01-22 17:26:13 +0000

    22/01

--- Calc.js
+++ Calc.js
@@ -1,8 +1,10 @@
-let currentInput = 0  
-let previousInput = 0
-let operator = 0 
+let currentInput = 0;
+let previousInput = 0;
+let operator = 0;
 
 const buttons = document.getElementById("grid")
+
+//Button creation + identifiers
 
 for (let i = 0; i < 15; i++) {
     const newDiv = document.createElement("div")
@@ -18,15 +20,18 @@ for (let i = 0; i < 15; i++) {
     } else if (i === 14) {
         newDiv.textContent = "C";
     } else if (i === 13) {
-        newDiv.textContent = "CE";
+        newDiv.textContent = ".";
     } else {
         newDiv.textContent = (i + 1).toString();
     }
+    newDiv.addEventListener("click", () => {
+        currentInput = (i + 1)
+    })
     buttons.appendChild(newDiv);
     console.log(newDiv)
 }
 
-
+// Operator Functions
 function add() {
     let sum = previousInput += currentInput
 
@@ -40,6 +45,10 @@ function multiply() {
 function divide() {
     let sum = previousInput /= currentInput
 }
+
+
+
+
 
 /*
 function operator(oper) {
