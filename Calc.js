cec606beeaecb633a88c27099c899e78def4e579
James McGonagle <jamesmcg08@hotmail.co.uk>
2025-02-09 01:02:06 +0000

    09/02 MVP

--- Calc.js
+++ Calc.js
@@ -1,6 +1,8 @@
 let currentInput = ""
 let previousInput = null
 let operator = null
+let output = 0
+
 
 
 const buttons = document.getElementById("grid")
@@ -18,11 +20,11 @@ for (let i = 0; i < 15; i++) {
     } else if (i === 11) {
         newDiv.textContent = "/";
     } else if (i === 12) {
-        newDiv.textContent = "=";
+        newDiv.textContent = "x";
     } else if (i === 14) {
         newDiv.textContent = "C";
     } else if (i === 13) {
-        newDiv.textContent = "CE";
+        newDiv.textContent = "=";
     } else {
         newDiv.textContent = (i + 1).toString();
     }
@@ -36,26 +38,56 @@ function handleButtonClick(value){
     if (!isNaN(value)){ //isNaN = "is not a number" so this makes sure it is a number with not not a number
         currentInput += value;
         updateDisplay(currentInput);
+        //console.log("1st: ", currentInput) Test for checking the current input
     }
-    else if (value === "+" || value === "-" || value ==="/"){
+    else if (value === "+" || value === "-" || value ==="/" || value === "x"){
         if(currentInput !== ""){
+            
             previousInput = currentInput;
             operator = value;
             currentInput = "";
-            updateDisplay(previousInput + "" + operator);
-            console.log(operator); //Test operator works 
+            
+            /**Highlight the selected operator - not working 
+            clearOperatorHighlight();
+            button.style.backgroundColor(rgb(160, 160, 160));
+            */
+
+            updateDisplay(previousInput + " " + operator);
+            //console.log(operator); Test operator works 
         }
     }
-    else if (value === "CE"){
-        updateDisplay("")
+    else if (value === "="){
+        if (operator === "+"){
+            output = parseFloat(previousInput) + parseFloat(currentInput)
+        }
+        else if (operator === "-"){
+            output = parseFloat(previousInput) - parseFloat(currentInput)
+        }
+        else if (operator === "/"){
+            output = previousInput / currentInput
+        }
+        else if (operator === "x"){
+            output = previousInput * currentInput
+        }
+        //console.log(output) test output
+        updateDisplay(output)
+        display.style.textAlign = 'right'
+
     }
     else if (value === "C") {
         updateDisplay("")
+        currentInput = ("")
         previousInput = null
         operator = null
+        display.style.textAlign = 'left'
     }
 }
 
+/**  Function to clear operator highlight - not working
+function clearOperatorHighlight() {
+    
+}
+*/
 
 
 // Function used to Display something within the Screen Div
